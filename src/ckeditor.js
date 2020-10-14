1421c2e53a1c587300e55a42373162e5fa530c07
Benji Smith <benji@benjismith.net>
2020-10-14 11:07:42 -0700

    Initial commit

---
+++ src/ckeditor.js
@@ -0,0 +1,158 @@
+/*
+   NOTE:
+
+   This file was copied from here...
+
+     https://github.com/ckeditor/ckeditor5-build-decoupled-document/blob/master/src/ckeditor.js
+   
+   ...but I added 'HorizontalLine', 'TodoList', and 'WordCount' plugins, and re-ordered the toolbar buttons
+ */
+
+/**
+ * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+// The editor creator to use.
+import DecoupledEditorBase from '@ckeditor/ckeditor5-editor-decoupled/src/decouplededitor';
+
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+import FontSize from '@ckeditor/ckeditor5-font/src/fontsize';
+import FontFamily from '@ckeditor/ckeditor5-font/src/fontfamily';
+import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor';
+import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor';
+import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
+import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
+import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+import Image from '@ckeditor/ckeditor5-image/src/image';
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
+import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent';
+import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock';
+import Link from '@ckeditor/ckeditor5-link/src/link';
+import List from '@ckeditor/ckeditor5-list/src/list';
+import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
+import Table from '@ckeditor/ckeditor5-table/src/table';
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+
+import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline';
+import TodoList from '@ckeditor/ckeditor5-list/src/todolist';
+import WordCount from '@ckeditor/ckeditor5-word-count/src/wordcount';
+
+export default class DecoupledEditor extends DecoupledEditorBase {}
+
+// TODO: external UI
+// https://ckeditor.com/docs/ckeditor5/latest/framework/guides/deep-dive/ui/external-ui.html
+
+// NOTE: document edtior?
+// https://ckeditor.com/docs/ckeditor5/latest/framework/guides/deep-dive/ui/document-editor.html
+
+// TODO: multi-root editor
+// https://ckeditor.com/docs/ckeditor5/latest/framework/guides/deep-dive/ui/custom-editor-creator.html
+
+// TODO: theme customization
+// https://ckeditor.com/docs/ckeditor5/latest/framework/guides/deep-dive/ui/theme-customization.html
+
+// LEARN MORE ABOUT: focus-tracking
+// https://ckeditor.com/docs/ckeditor5/latest/framework/guides/deep-dive/ui/focus-tracking.html
+
+// LEARN MORE ABOUT: widgets
+// https://ckeditor.com/docs/ckeditor5/latest/framework/guides/deep-dive/ui/widget-internals.html
+
+// CUSTOMIZING EDITOR OUTPUT
+// https://ckeditor.com/docs/ckeditor5/latest/framework/guides/deep-dive/conversion/conversion-extending-output.html
+
+// Plugins to include in the build.
+DecoupledEditor.builtinPlugins = [
+  Essentials,
+  Alignment,
+  FontSize,
+  FontFamily,
+  FontColor,
+  FontBackgroundColor,
+  UploadAdapter,
+  Autoformat,
+  Bold,
+  Italic,
+  Strikethrough,
+  Underline,
+  BlockQuote,
+  CKFinder,
+  EasyImage,
+  Heading,
+  HorizontalLine,
+  Image,
+  ImageCaption,
+  ImageStyle,
+  ImageToolbar,
+  ImageUpload,
+  Indent,
+  IndentBlock,
+  Link,
+  List,
+  MediaEmbed,
+  Paragraph,
+  PasteFromOffice,
+  Table,
+  TableToolbar,
+  TextTransformation,
+  TodoList,
+  WordCount
+];
+
+// Editor configuration.
+DecoupledEditor.defaultConfig = {
+  toolbar: {
+    items: [
+      'heading',
+      '|',
+      'bold', 'italic', 'underline', 'strikethrough',  'subscript',  'superscript',
+      '|',
+      'alignment',
+      '|',
+      'indent', 'outdent', 'numberedList', 'bulletedList', 'todoList', 'blockquote', 'horizontalLine',
+      '|',
+      'fontfamily', 'fontsize', 'fontColor', 'fontBackgroundColor',
+      '|',
+      'link', 'imageUpload', 'insertTable', 'mediaEmbed',
+      '|',
+      'undo', 'redo'
+    ]
+  },
+  image: {
+    styles: [
+      'full',
+      'alignLeft',
+      'alignRight'
+    ],
+    toolbar: [
+      'imageStyle:alignLeft',
+      'imageStyle:full',
+      'imageStyle:alignRight',
+      '|',
+      'imageTextAlternative'
+    ]
+  },
+  table: {
+    contentToolbar: [
+      'tableColumn',
+      'tableRow',
+      'mergeTableCells'
+    ]
+  },
+  // This value must be kept in sync with the language defined in webpack.config.js.
+  language: 'en'
+};
---
+++ webpack.config.js
@@ -0,0 +1,96 @@
+/*
+   NOTE:
+
+   This file was copied from here...
+
+     https://github.com/ckeditor/ckeditor5-build-decoupled-document/blob/master/webpack.config.js
+   
+   ...but I removed the `additionalLanguages: 'all'` param from the CKEditorWebpackPlugin args
+ */
+
+/**
+ * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+'use strict';
+
+const path = require( 'path' );
+const webpack = require( 'webpack' );
+const { bundler, styles } = require( '@ckeditor/ckeditor5-dev-utils' );
+const CKEditorWebpackPlugin = require( '@ckeditor/ckeditor5-dev-webpack-plugin' );
+const TerserPlugin = require( 'terser-webpack-plugin' );
+
+module.exports = {
+  devtool: 'source-map',
+  performance: { hints: false },
+
+  entry: path.resolve( __dirname, 'src', 'ckeditor.js' ),
+
+  output: {
+    // The name under which the editor will be exported.
+    library: 'DecoupledEditor',
+
+    path: path.resolve( __dirname, 'build' ),
+    filename: 'ckeditor.js',
+    libraryTarget: 'umd',
+    libraryExport: 'default'
+  },
+
+  optimization: {
+    minimizer: [
+      new TerserPlugin( {
+        sourceMap: true,
+        terserOptions: {
+          output: {
+            // Preserve CKEditor 5 license comments.
+            comments: /^!/
+          }
+        },
+        extractComments: false
+      } )
+    ]
+  },
+
+  plugins: [
+    new CKEditorWebpackPlugin( {
+      language: 'en'
+    } ),
+    new webpack.BannerPlugin( {
+      banner: bundler.getLicenseBanner(),
+      raw: true
+    } )
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        use: [ 'raw-loader' ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+            options: {
+              injectType: 'singletonStyleTag',
+              attributes: {
+                'data-cke': true
+              }
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: styles.getPostCssConfig( {
+              themeImporter: {
+                themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' )
+              },
+              minify: true
+            } )
+          }
+        ]
+      }
+    ]
+  }
+};
