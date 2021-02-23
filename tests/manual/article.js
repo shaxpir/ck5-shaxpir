dbecfa4d5d8cd69e7d17371e5eca19c8b402946a
Marek Lewandowski <m.lewandowski@cksource.com>
2021-02-23 15:15:35 +0100

    Added a manual test.

---
+++ tests/manual/article.js
@@ -0,0 +1,58 @@
+/**
+ * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+/* globals console, window, document */
+
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+
+import ArticlePluginSet from '@ckeditor/ckeditor5-core/tests/_utils/articlepluginset';
+import { ShaxpirSentimentPlugin } from '../../src/plugins/ShaxpirSentimentPlugin';
+
+ClassicEditor
+	.create( document.querySelector( '#editor' ), {
+		plugins: [ ArticlePluginSet, ShaxpirSentimentPlugin ],
+		toolbar: [
+			'sentiment',
+			'heading',
+			'|',
+			'bold',
+			'italic',
+			'link',
+			'bulletedList',
+			'numberedList',
+			'|',
+			'outdent',
+			'indent',
+			'|',
+			'blockQuote',
+			'insertTable',
+			'mediaEmbed',
+			'undo',
+			'redo'
+		],
+		sentiment: {
+			getSentimentForWord: () => ( {
+				score: 7.2,
+				label: 'moderately positive',
+				color: 'darkgreen'
+			} )
+		},
+		image: {
+			toolbar: [ 'imageStyle:full', 'imageStyle:side', '|', 'imageTextAlternative' ]
+		},
+		table: {
+			contentToolbar: [
+				'tableColumn',
+				'tableRow',
+				'mergeTableCells'
+			]
+		}
+	} )
+	.then( editor => {
+		window.editor = editor;
+	} )
+	.catch( err => {
+		console.error( err.stack );
+	} );
